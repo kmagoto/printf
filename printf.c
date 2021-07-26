f88e6c97ed8260be1c1489879b6634edc8c7a999
kmagoto <kennedymagoto@gmail.com>
2021-07-26 08:00:51 -0700

    printf function

--- printf.c
+++ printf.c
@@ -1,34 +1,50 @@
 #include "holberton.h"
 
 /**
- * _printf - prints a formatted string
- * @format: string with desired characters
- * Return: printed characters
+ * _printf - main func that prints in console
+ * @format: array to print and check type
+ * Return: count of characters printed
  *
  */
 
 int _printf(const char *format, ...)
 {
-  int printed_chars;
-  convert_t f_list[] = {
-			{"c", print_char},
-			{"s", print_string},
-			{"%", print_percent},
-			{"d", print_integer},
-			{"i", print_integer},
-			{NULL, NULL}
-  };
-  va_list arg_list;
+	int count = -1;
 
-  if (format == NULL)
-    return (-1);
+	if (format != NULL)
+	{
+		int i;
+		va_list ar_list;
+		int (*o)(va_list);
 
-  return (printed_chars);
+		va_start(ar_list, format);
 
-  va_start(arg_list, format);
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
 
-  /* call the parser function*/
-  printed_chars = parser(format, f_list, arg_list);
-  va_end(arg_list);
-  return (printed_chars);
+		count = 0;
+
+		for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i++;
+				}
+				else if (format[i + 1] != '\0')
+				{
+					o = get_func(format[i + 1]);
+					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
+					i++;
+				}
+			}
+			else
+				count += _putchar(format[i]);
+		}
+		va_end(ar_list);
+	}
+
+	return (count);
 }
