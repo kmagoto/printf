dad8a2cd3420293987dad82b6465e1615621171a
kmagoto <kennedymagoto@gmail.com>
2021-07-26 07:34:58 -0700

    resources

---
+++ holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+int _printf(const char *format, ...);
+
+
+
+
+
+#endif
