6783c4f719dd00f7ba3ce4dd560326d49e71e683
kmagoto <kennedymagoto@gmail.com>
2021-07-26 08:00:23 -0700

    resources

--- holberton.h
+++ holberton.h
@@ -1,12 +1,16 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <limits.h>
+
 
 int _printf(const char *format, ...);
+int _putchar(char c);
 
 
 
